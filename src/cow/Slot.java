43f9181e32a82454ca0faefd3a034d5a55c733d4
Artem Sinchuk <158162880+ArtemSinchuk@users.noreply.github.com>
2024-07-31 14:31:43 +0200

    Added Slot

---
+++ src/cow/Slot.java
@@ -0,0 +1,20 @@
+// package milkandmeadows.cow;
+public class Slot {
+  private Item item;
+
+  public Slot() {
+      this.item = null;
+  }
+
+  public Item getItem() {
+      return item;
+  }
+
+  public void setItem(Item item) {
+      this.item = item;
+  }
+
+  public boolean isEmpty() {
+      return item == null || item.getQuantity() == 0;
+  }
+}
